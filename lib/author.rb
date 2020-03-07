e6291ef996c28e29fdca4bcc6b3904be7c6756bb
Philip Lee-Chin <phil.leechin@outlook.com>
2020-03-07 19:44:47 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -3,10 +3,18 @@ class Author
 
   def initialize(au_name)
     @name = au_name
-    @post = []
+    @posts = []
   end
 
   def posts
     Post.all
   end
+
+  def add_post(post)
+    post.author = self
+    @posts.each do |i|
+      i << post
+    end
+  end
+
 end
