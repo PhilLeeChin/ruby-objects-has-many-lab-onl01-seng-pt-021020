9a611c611f2abab8819c5c9efac1875122bff495
Philip Lee-Chin <phil.leechin@outlook.com>
2020-03-07 19:20:54 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -18,9 +18,9 @@ class Artist
   end
 
   def add_song_by_name(name)
-    song = Song.new(name)
-    self.songs << song
-    song.artist = self
+    songs = Song.new(name)
+    self.songs << songs
+    songs.artist = self
     @@song_count += 1
   end
 
